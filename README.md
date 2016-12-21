7e4666ef06341b7b6ed919a03ff99f94e07c47e6
claudia <clou.7loymed@gmail.com>
2016-12-21 12:43:10 -0500

    javasS

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # Dormimos---Ej2---S17
+Inicio: Evaluar si el usuario puede dormir
+
+## Ingreo de datos
+- weeday
+- vacations
+- sleeping
+
+## Proceso
+Si vacations is true 
+    Mostrar 1//sleeping
+Si no
+    si weekday is true
+    Mostrar 0//No sleeping
+      
+    Si no
+     Mostrar 1//Dormimos
+    Fin si
+    
+Fin si
+
+*Fin*
---
+++ main.js
@@ -0,0 +1,30 @@
+function calcular()
+{
+    var vacation = document.getElementById("vacaciones").value;
+    var weekday = parseInt(document.getElementById("dia")).value;
+    
+    var isVacation = false;
+    var isWeekday = false;
+    
+    var salida = document.getElementById("salida");
+    
+    if(vacation == "2")
+        {
+            isVacation = false;
+        }
+    if(weekday < 6)
+        {
+            isWeekday = true;
+        }
+    if(isVacation)
+        {
+            salida.innerHTML = "SI";
+        }else{
+            if(isWeekday)
+                {
+                    salida.innerHTML = "No";
+                }else{
+                    salida.innerHTML = "Si";
+                }
+        }
+}
